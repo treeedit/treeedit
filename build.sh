f16048d9f428a99b63c2f15c5b456f6c19de0e4d
Tree Edit <info@treeedit.org>
2024-04-25 14:49:56 -0700

    Codesign app

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@
 rm -rf build
 qt-cmake -S . -B build
 cmake --build build
+
 pushd build/test
 ./treeedit-test
 popd
+
 pushd build/src
-macdeployqt treeedit.app -dmg
+macdeployqt treeedit.app -codesign="Mac Developer: Paul Borza (976ZM2SB6H)" -dmg
 popd
